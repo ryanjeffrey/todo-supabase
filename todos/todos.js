55c36e4aa0af82b644d624006bf33eee87ce71bc
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-23 13:00:25 -0500

    Receive input from the form, and use that data to display new todos in DOM

--- todos/todos.js
+++ todos/todos.js
@@ -15,8 +15,25 @@ const todoForm = document.querySelector('.todo-form');
 const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
-todoForm.addEventListener('submit', async(e) => {
+todoForm.addEventListener('submit', async (e) => {
     // on submit, create a todo, reset the form, and display the todos
+    e.preventDefault();
+
+    const formData = new FormData(todoForm);
+    const todo = formData.get('todo');
+
+    const response = await createTodo(todo);
+    console.log(response);
+
+    todoForm.reset();
+
+    const error = response.error;
+
+    if (error) {
+        console.log(error.message);
+    } else {
+        displayTodos();
+    }
 });
 
 // create todo state
@@ -25,26 +42,39 @@ todoForm.addEventListener('submit', async(e) => {
     // call completeTodo
     // swap out todo in array
     // call displayTodos
-
    
 
 async function displayTodos() {
     // clear the container (.innerHTML = '')
+    todosEl.innerHTML = '';
     // display the list of todos, 
-          // call render function, pass in state and complete handler function!
-          // append to .todos
+    const todos = await getTodos();
+    for (let todo of todos) {
+        // call render function, pass in state and complete handler function!
+        const renderedTodo = renderTodo(todo);
+        // append to .todos
+        todosEl.append(renderedTodo);
+    }
+   
 }
 
 // add page load function
+
+
+async function loadData() {
     // fetch the todos and store in state
     // call displayTodos
+    displayTodos();
+}
+
+loadData();
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
 
 
-deleteButton.addEventListener('click', async() => {
+deleteButton.addEventListener('click', async () => {
     // delete all todos
     // modify state to match
     // re displayTodos
