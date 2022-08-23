e23735c6b1d6564e68916dedad37312de5e9a500
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-23 12:58:59 -0500

    Create functions for inserting a todo and fetching all todos from supabase

--- fetch-utils.js
+++ fetch-utils.js
@@ -6,7 +6,10 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function createTodo(todo) {
     // create a single incomplete todo with the correct 'todo' property for this user in supabase
-
+    const response = await client.from('todos').insert({
+        todo: todo,
+        complete:false,
+        user_id: client.auth.user().id }).single();
     return checkError(response);
 }
 
@@ -18,7 +21,7 @@ export async function deleteAllTodos() {
 
 export async function getTodos() {
     // get all todos for this user from supabase
-
+    const response = await client.from('todos').select('*');
     return checkError(response);
 }
 
